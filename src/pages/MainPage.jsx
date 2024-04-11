6c22500922235374b7549ee65817c0387e428906
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-04-11 11:17:57 +0530

    NavigationBar Added

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,7 +4,7 @@ export default function MainPage() {
     return (
         <div>
             <NavigationBar />
-            <h1 className="txt">My Portfolio</h1>
+            <h1>My Portfolio</h1>
         </div>
     );
 }
