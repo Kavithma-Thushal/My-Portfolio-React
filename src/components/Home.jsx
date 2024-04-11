50c548f3386fca5f86423574808cd6176d2ac45c
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-04-11 20:11:50 +0530

    Nav Bar Updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import Profile from "../assets/images/profile.jpg";
 
 export default function Home() {
     return (
-        <Container className="mt-5">
+        <Container className="mt-5" fixed="top">
             <Row className="align-items-center">
                 <Col md={4}>
                     <img src={Profile} alt="Profile" className="img-fluid rounded-circle" />
@@ -11,7 +11,7 @@ export default function Home() {
                 <Col md={8}>
                     <h1 className="text-white">I'm Kavithma Thushal</h1>
                     <h2 className="text-white">FullStack Engineer</h2>
-                    <Button>Hire Me</Button>
+                    <Button variant="primary">Hire Me</Button>
                 </Col>
             </Row>
         </Container>
