c8d134dd2a9574a2a49e2ee9575e7d142da1fb9d
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-04-13 07:33:17 +0530

    Project Structure Added

---
+++ src/components/Projects.jsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+    return (
+        <div>
+            <h1 className="txt-head">My Projects</h1>
+            <div className="project-border"></div>
+        </div>
+    );
+}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,13 @@
 import NavigationBar from "../components/NavigationBar";
 import Home from "../components/Home";
+import Projects from "../components/Projects";
 
 export default function MainPage() {
     return (
         <div>
             <NavigationBar />
             <Home />
+            <Projects />
         </div>
     );
 }
