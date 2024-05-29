7cdb2165423f607399be54dc43f219c011e976f0
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-05-30 00:18:33 +0530

    Tailwind CSS Added

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
     return (
         <div>
-            <h1>My-Portfolio-React</h1>
+            <h1 className={"text-red-500"}>My-Portfolio-React</h1>
         </div>
     );
 }
