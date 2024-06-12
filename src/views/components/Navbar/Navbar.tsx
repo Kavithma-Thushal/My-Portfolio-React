17ced71d14d625fe6263a42fc23ffbd6c8b4b62e
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-12 23:25:58 +0530

    Navbar.tsx Refactored 2

--- src/views/components/Navbar/Navbar.tsx
+++ src/views/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ export function Navbar() {
     return (
         <nav className="bg-darkBlue text-white p-2 pr-7">
             <div className="container mx-auto flex justify-between items-center">
-                <a href="https://github.com/Kavithma-Thushal/My-Portfolio-React.git" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/Kavithma-Thushal/My-Portfolio-React.git" target="_blank">
                     <div className="flex items-center">
                         <img src={logo} className="Nav-logo mr-2" alt="Nav-logo"/>
                         <div className="text-lg font-bold">P O R T F O <span className="text-neonBlue">L I O</span>
