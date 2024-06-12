8d2b5c6edde1b2f242d11ed2a95f3812f0f3e44b
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-12 23:16:22 +0530

    Navbar.tsx Refactored

--- src/views/components/Footer/Footer.tsx
+++ src/views/components/Footer/Footer.tsx
@@ -2,8 +2,8 @@ export function Footer() {
     return (
         <footer className="bg-darkBlue text-white p-3">
             <h6 className="text-center">© 2024 All Right Reserved. Designed By <a
-                href="https://github.com/Kavithma-Thushal" target={"_blank"}
-                className="font-semibold hover:text-neonBlue transition-colors duration-300">Kavithma Thushal</a></h6>
+                href="https://github.com/Kavithma-Thushal/My-Portfolio-React.git" target={"_blank"}
+                className="font-semibold text-neonBlue transition-colors duration-300">Kavithma Thushal</a></h6>
         </footer>
     );
 }
