e2da021116af4f55b2569cf9d4f67f789ee21a70
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-13 13:22:18 +0530

    Content Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import {Navbar} from "./views/components/Navbar/Navbar";
+import {Content} from "./views/components/Content/Content";
 import {Footer} from "./views/components/Footer/Footer";
-import {Home} from "./views/pages/Home/Home";
-import {About} from "./views/pages/About/About";
-import {Skills} from "./views/pages/Skills/Skills";
-import {Projects} from "./views/pages/Projects/Projects";
-import {Services} from "./views/pages/Services/Services";
-import {Contacts} from "./views/pages/Contacts/Contacts";
 
 export default function App() {
     return (
         <div>
             <Navbar/>
-            <Home/>
-            <About/>
-            <Skills/>
-            <Projects/>
-            <Services/>
-            <Contacts/>
+            <Content/>
             <Footer/>
         </div>
     );
---
+++ src/views/components/Content/Content.tsx
@@ -0,0 +1,19 @@
+import {Home} from "../../pages/Home/Home";
+import {About} from "../../pages/About/About";
+import {Skills} from "../../pages/Skills/Skills";
+import {Projects} from "../../pages/Projects/Projects";
+import {Services} from "../../pages/Services/Services";
+import {Contacts} from "../../pages/Contacts/Contacts";
+
+export function Content() {
+    return (
+        <div>
+            <Home/>
+            {/*<About/>*/}
+            {/*<Skills/>*/}
+            {/*<Projects/>*/}
+            {/*<Services/>*/}
+            {/*<Contacts/>*/}
+        </div>
+    );
+}
