85285903a52636274fffe526aa1bac4aaef63e73
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-13 17:28:10 +0530

    All Cards Added

--- src/views/components/Card/Card.tsx
+++ src/views/components/Card/Card.tsx
@@ -1,12 +1,18 @@
 interface CardProps {
     title: string;
+    img: string
 }
 
-export function Card({title}: CardProps) {
+export function Card({title, img}: CardProps) {
     return (
-        <div className="text-center m-5 p-5 rounded-lg bg-black border border-blue-300 shadow-2xl w-60">
+        <div className="text-center m-5 p-5 rounded-lg bg-black border border-[#00c7ff] shadow-2xl w-60">
             <h1 className="text-3xl font-semibold mb-5">{title}</h1>
-            <button className="text-black font-semibold bg-neonBlue px-3 py-1 text-sm rounded">Explore</button>
+            <div className={"flex items-center justify-center"}>
+                <img className={"w-28 mb-6"} src={img}/>
+            </div>
+            <button
+                className="text-white font-semibold hover:bg-neonBlue hover:text-black px-3 py-1 rounded border border-[#00c7ff] transition duration-300 w-full">Explore
+            </button>
         </div>
     );
 }
--- src/views/pages/Projects/Projects.tsx
+++ src/views/pages/Projects/Projects.tsx
@@ -1,18 +1,26 @@
 import {Card} from "../../components/Card/Card";
+import java from "../../../assets/images/projects/languages/java.png";
+import javafx from "../../../assets/images/projects/languages/javafx.png";
+import spring from "../../../assets/images/projects/languages/spring.png";
+import springboot from "../../../assets/images/projects/languages/spring.png";
+import html from "../../../assets/images/projects/languages/html.png";
+import css from "../../../assets/images/projects/languages/css.png";
+import js from "../../../assets/images/projects/languages/js.png";
+import react from "../../../assets/images/projects/languages/react.png";
 
 export function Projects() {
     return (
         <div className="bg-darkBlue text-white p-10">
             <h1 className="text-4xl font-bold mb-8 text-center">My Projects</h1>
             <div className="flex flex-wrap justify-center gap-5">
-                <Card title="Java"/>
-                <Card title="JavaFX"/>
-                <Card title="Spring"/>
-                <Card title="SpringBoot"/>
-                <Card title="HTML"/>
-                <Card title="CSS"/>
-                <Card title="JavaScript"/>
-                <Card title="React"/>
+                <Card title="Java" img={java}/>
+                <Card title="JavaFX" img={javafx}/>
+                <Card title="Spring" img={spring}/>
+                <Card title="SpringBoot" img={springboot}/>
+                <Card title="HTML" img={html}/>
+                <Card title="CSS" img={css}/>
+                <Card title="JS" img={js}/>
+                <Card title="React" img={react}/>
             </div>
         </div>
     );
